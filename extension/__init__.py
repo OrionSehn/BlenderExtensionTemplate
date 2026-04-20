e932b5abb3631f4b8dbd2005ef234f757daf6f1c
Orion Sehn <OTSehn@pcl.com>
2026-04-20 09:22:41 -0600

    added build task for using the blender development extension

--- extension/__init__.py
+++ extension/__init__.py
@@ -1,30 +1,35 @@
 import bpy
 
-# from foo import hello_world # to have it available in the imported namespace
-import ext_name.submod1
+from ext_name.submod1.foo import hello_world # to have it available in the imported namespace
+import ext_name
 
 
-# __all__ = [
-#     "submod1",
-#     "hello_world"
-#     ]
+__all__ = [
+    "submod1",
+    "hello_world"
+    ]
 
-# def __dir__():
-#     return __all__
+def __dir__():
+    return __all__
 
-# from ext_name.submod1 import foo
+from ext_name.submod1 import foo
 
-# classes = [
-#     foo.HelloWorldOperator
-# ]
+classes = [
+    foo.HelloWorldOperator
+]
 
 
 def register():
     print("Registering extension...") 
     print("Available submodules:", dir(ext_name))
-    # print("Available further submodules:", dir(ext_name.ext_name))
-    # print("Available EVEN further submodules:", dir(ext_name.ext_name.ext_name))
+    print("__all__:", __all__)
+
+    print("registering operator")
+    bpy.utils.register_class(foo.HelloWorldOperator)
+
+    print("Extension registered successfully.🔥🚁🚁")
 
 def unregister():
     print("Unregistering extension...") 
     # hello_world()
+    bpy.utils.unregister_class(foo.HelloWorldOperator)
